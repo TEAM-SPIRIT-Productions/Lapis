4a8eb977ec6375ccb39fd53bb0e1d3f10cca7e43
Bratah123 <brandonnguyen3301@gmail.com>
2021-01-01 13:33:51 -0800

    FEAT: Add info command
    - Tested to be working 100%

--- src/commands/user_commands.py
+++ src/commands/user_commands.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+from lazuli.database import Lazuli
+import json
+import os
 
 
 class UserCommands(commands.Cog, name='UserCommands'):
@@ -10,9 +13,51 @@ class UserCommands(commands.Cog, name='UserCommands'):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='hello', pass_context=True)
-    async def hello_world(self, ctx, *, member: discord.Member = None):
-        await ctx.send('Hello from Lapis!')
+        with open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/config.json') as f:
+            self.config = json.load(f)
+
+        self.database = Lazuli()
+        # Lazuli will automatically connect to localhost
+        # If config is not using default then we change it to custom settings
+        if not self.config['USE_DEFAULT_DB']:
+            self.database = Lazuli(
+                host=self.config['DATABASE_HOST'],
+                schema=self.config['DATABASE_NAME'],
+                user=self.config['DATABASE_USER'],
+                password=self.config['DATABASE_CONFIG'],
+                port=self.config['DATABASE_PORT'],
+            )
+
+    def get_server_color(self):
+        """
+        Converting the hex string in config file to a hex int for color embed
+        :return: int
+        """
+        return int(self.config['SERVER_COLOR'], 16) + 0x200
+
+    @commands.command(name='info', pass_context=True)
+    async def info(self, ctx, *, member: discord.Member = None):
+        # Change the stats here for your server
+        info_stat = {
+            'Exp': '1x',
+            'Item': '1x',
+            'Meso': '1x',
+            'Server State': 'N/A',
+            'Server Location': 'N/A',
+            'Release Date': 'N/A',
+        }
+        embed = discord.Embed(
+            title=self.config['SERVER_NAME'],
+            color=self.get_server_color(),
+            description="KMS v316"
+        )
+        embed.set_thumbnail(url=self.config['SERVER_IMG'])
+
+        for stat in info_stat:
+            embed.add_field(name=stat, value=info_stat[stat], inline=True)
+
+        embed.set_footer(text=self.config['SERVER_NAME'])
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
